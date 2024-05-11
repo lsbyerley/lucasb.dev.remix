0d01792558f0cd4265dbba68808cad6ae4fafd5d
lsbyerley <lsbyerley@gmail.com>
2024-05-11 09:56:18 -0400

    chore: add project

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -65,30 +65,32 @@ const Home = () => {
 								alt="Project 1"
 								className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
 								height="310"
-								src="https://generated.vusercontent.net/placeholder.svg"
+								src="/img/ddscreenshot.png"
 								width="550"
 							/>
 							<div className="flex flex-col justify-center space-y-4">
 								<div className="space-y-2">
-									<h3 className="text-2xl font-bold">Project 1</h3>
+									<h3 className="text-2xl font-bold">Daily Deals</h3>
 									<p className="text-gray-500 dark:text-gray-400">
-										A modern web application built with React, Next.js, and
-										Tailwind CSS.
+										A modern web application built with React, Next.js,
+										Tailwind CSS, and Supabase. Deployed to Vercel
 									</p>
 								</div>
 								<div className="flex flex-col items-center justify-center gap-2 min-[400px]:flex-row">
-									<Link
+									<a
 										className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-										to="/"
+										href="https://dailydeals.vercel.app"
+										rel="noreferrer nofollow"
 									>
 										View Project
-									</Link>
-									<Link
+									</a>
+									<a
 										className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-										to="/"
+										href="https://github.com/lsbyerley/daily-deals"
+										rel="noreferrer nofollow"
 									>
 										GitHub
-									</Link>
+									</a>
 								</div>
 							</div>
 						</div>
